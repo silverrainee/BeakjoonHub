ab35bb8f1701ae0c8874c2e368f17769a944531a
silverrainee <109517776+silverrainee@users.noreply.github.com>
2023-02-06 23:36:09 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,13 +1,12 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 int main() {
-    
-    while(1){
-        int a, b;
-        cin >>a>>b;
-        if(!a&&!b) return 0;
-        cout << a+b <<"\n";
-    }
+	while (true) {
+		int A, B;
+		cin >> A >> B;
+		if (!A && !B) break;
+		cout << A + B << "\n";
+	}
 }
