5c80a277cee807902f46c4df0c4067b06ec32486
silverrainee <109517776+silverrainee@users.noreply.github.com>
2023-02-13 21:35:13 +0900

    [Silver I] Title: 격자상의 경로, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
 int N; // 맵의 높이
 int M; // 맵의 길이
-int circle_num; // 동그라미 숫자
+int num; // 동그라미 숫자
 
 int ans[16][16];
 int dp(int y, int x) {
@@ -17,16 +16,16 @@ int dp(int y, int x) {
 }
 
 int main() {
-	cin >> N >> M >> circle_num;
+	cin >> N >> M >> num;
 
-	if (circle_num == 0) {
+	if (num == 0) {
 		cout << dp(N, M);
 
 		return 0;
 	}
 
-	int n = (circle_num - 1) / M + 1;
-	int m = (circle_num - 1) % M + 1;
+	int n = (num - 1) / M + 1;
+	int m = (num - 1) % M + 1;
 
 	cout << dp(n, m) * dp(N - n + 1, M - m + 1);
 
